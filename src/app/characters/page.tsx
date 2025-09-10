d0ad51642efde68759a1994f7b2390e70ed82abc
KomaUki <komauki@yandex.ru>
2025-09-10 21:07:29 +0300

    исрпавляем пиздец какой косяк3

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -88,7 +88,7 @@ function CharactersPageContent() {
         )}
         {/* Characters Grid */}
         {!loading && !error && (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-6 auto-rows-[350px]">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-6">
             {characters.map((character) => (
               <CharacterCard key={character.id} character={character} />
             ))}
